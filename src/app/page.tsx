ea38fdff408e414443f4c02f46584725f8772562
Attila Reterics <reterics.attila@gmail.com>
2023-09-22 20:54:58 +0200

    Import Export added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import Image from 'next/image'
 import React, {useRef, useState} from "react";
 import {DEFAULTS, inputSides, inputTypes, ItemInput, tailwindCSS} from "@/app/itemInput";
 import {SVGDesigner} from "@/app/designer";
+import {V1} from "@/app/lib/V1";
 
 export default function Home() {
   const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
   const [absoluteEditor, setAbsoluteEditor] = useState(DEFAULTS.absoluteEditor);
+  const [squareMeter, setSquareMeter] = useState(0)
+  const [squareMeterCalculated, setSquareMeterCalculated] = useState(0)
 
   const addItemToList = (item) => {
     setItems([...items, item]);
@@ -25,25 +29,94 @@ export default function Home() {
     });
     setItems([...items]);
   }
+
   const handleChange = () => {
     setAbsoluteEditor(!absoluteEditor)
   };
+
+  const exportData = () => {
+    V1.downloadAsFile(name, JSON.stringify(items), 'application/json');
+  }
+
+  const importData = async function () {
+    const file = await V1.readTextFile();
+    if (file && typeof file.value === "string") {
+      const json = JSON.parse(file.value);
+      console.log(json);
+
+      if (Array.isArray(json)) {
+        setItems(json);
+      }
+    }
+  }
+
+  function changeName(e) {
+    setName(e.target.value);
+  }
+
+  function reset() {
+    setItems([]);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Welcome to &nbsp;
           <code className="font-mono font-bold">Building Structure Calculator</code>
         </p>
 
-        <div>
+        <div className="flex">
+          <div className="flex h-[40px]">
+            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-[50px] p-2">Name:</label>
+
+            <input type="name" id="name"
+                   defaultValue={name}
+                   className="mr-8 max-w-[100px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   placeholder="Project Name" onChange={changeName} />
+
+            <input type="m21" id="m21"
+                   className="max-w-[100px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   placeholder={squareMeter.toString()} />
+            <label htmlFor="m21" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-[50px] p-2">m2 ></label>
+
+            <input type="m22" id="m22"
+                   className="max-w-[100px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   placeholder={squareMeterCalculated.toString()} />
+            <label htmlFor="m22" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-[40px] p-2">m2</label>
+
+
+            <div className="inline-flex rounded-md shadow-sm h-[40px]" role="group">
+              <button onClick={()=>reset()} type="button"
+                      className="px-4 text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                Reset
+              </button>
+              <button onClick={()=>importData()} type="button"
+                      className="px-4 text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                Import
+              </button>
+              <button onClick={()=>exportData()} type="button"
+                      className="px-4 text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                Export
+              </button>
+            </div>
+
+          </div>
+
+
+        </div>
+        <div className="block hidden">
+
+
           <div className="relative inline-flex items-center cursor-pointer" onClick={handleChange}>
             <input type="checkbox" value="" className="sr-only peer"
                    defaultChecked={absoluteEditor}/>
-              <div
-    className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"/>
-              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Absolute Editor</span>
+            <div
+                className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"/>
+            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Absolute Editor</span>
           </div>
+
+
         </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -67,9 +140,9 @@ export default function Home() {
 
 
         <div className="overflow-x-auto w-full  h-[76vh]">
-            <div className="float-left w-[795px]">
+            <div className="float-left w-[790px]">
               <div className="overflow-x-auto inline-block" style={{maxHeight:'76vh'}}>
-                <table className="min-w-full text-left text-sm font-light max-w-[795px]">
+                <table className="min-w-full text-left text-sm font-light max-w-[790px]">
                   <thead className="border-b font-medium dark:border-neutral-500">
                   <tr>
                     <th scope="col" className="px-6 py-4 hidden">#</th>
@@ -80,6 +153,8 @@ export default function Home() {
                     <th scope="col" className={absoluteEditor ? "px-6 py-4" : "px-6 py-4 hidden"}>Offset 2</th>
                     <th scope="col" className="px-6 py-4">Min</th>
                     <th scope="col" className="px-6 py-4">Max</th>
+                    <th scope="col" className="px-6 py-4">Calculated</th>
+
                   </tr>
                   </thead>
                   <tbody>
@@ -115,10 +190,13 @@ export default function Home() {
                         <td className="whitespace-nowrap px-6 py-4 min-w-[98px]">{item.minLength} cm</td>
                         <td className="whitespace-nowrap px-6 py-4 min-w-[98px]">{item.type !== inputTypes[1] ?
                             item.maxLength + 'cm' :'-'} </td>
+                        <td className="whitespace-nowrap px-6 py-4 min-w-[98px]">{item.calculated || item.minLength} cm</td>
+
                       </tr>
                   ))}
                   </tbody>
                 </table>
+
               </div>
             </div>
           <div className="overflow-x-auto float-left h-full" style={{width:'calc(100% - 790px)'}}>
